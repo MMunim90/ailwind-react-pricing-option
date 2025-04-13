6edde257bd2b06ae6fab5ca23f06f4729cd4f070
MMunim <mmunim9.9.01@gmail.com>
2025-04-13 16:36:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+import Navbar from "./components/Navbar/Navbar";
+
+
+function App() {
+  return (
+    <>
+      <header>
+        <DaisyNav/>
+        <Navbar></Navbar>
+      </header>
+      <main>
+
+      </main>
+    </>
+  );
+}
+
+export default App;
